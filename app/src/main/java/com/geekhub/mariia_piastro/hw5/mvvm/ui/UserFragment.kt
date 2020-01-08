7666dbf154ec05f1afb4f0576a83feb9cfc91bc2
MariiaPiastro <mashadia4@gmail.com>
2020-01-08 17:01:03 +0200

    observe

--- app/src/main/java/com/geekhub/mariia_piastro/hw5/mvvm/ui/UserFragment.kt
+++ app/src/main/java/com/geekhub/mariia_piastro/hw5/mvvm/ui/UserFragment.kt
@@ -5,12 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.geekhub.mariia_piastro.hw5.mvvm.R
 import com.geekhub.mariia_piastro.hw5.mvvm.model.User
+import com.geekhub.mariia_piastro.hw5.mvvm.viewModel.UserViewModel
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_user.view.*
 
 class UserFragment : Fragment() {
+
+    private var model: UserViewModel? = null
+    private var mUser: User? = null
+    private var login = ""
+
 
     companion object {
         fun newInstance(user: User): UserFragment {
@@ -22,12 +30,23 @@ class UserFragment : Fragment() {
         }
     }
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (arguments != null) {
+            login = arguments!!.getString("login")!!
+        }
+        model = ViewModelProviders.of(requireActivity()).get(UserViewModel::class.java)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        model?.load(login)?.observe(this, Observer { it ->
+            mUser = it
+        })
         return inflater.inflate(R.layout.fragment_user, container, false)
-    }
+           }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -42,7 +61,6 @@ class UserFragment : Fragment() {
             tvLocation.text = String.format("Location: ", userDetail?.location)
             tvRegistration.text = String.format("URL: ", userDetail?.registration)
             Picasso.get().load(userDetail?.avatarUrl).into(view.imageViewAvatar)
-
         }
     }
 }
